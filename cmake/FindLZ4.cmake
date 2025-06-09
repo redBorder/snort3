2704c169522d0ce95756d69053913dbc4b86e79c
Miguel Alvarez <malvarez@redborder.com>
2025-06-09 12:37:50 +0100

    Fix link to lz4

--- cmake/FindLZ4.cmake
+++ cmake/FindLZ4.cmake
@@ -1,35 +1,46 @@
-find_package(PkgConfig REQUIRED)
+find_package(PkgConfig)
 pkg_check_modules(PC_LZ4 lz4)
 
 if (PC_LZ4_FOUND)
-    set(LZ4_LIBRARIES ${PC_LZ4_LIBRARIES})
-    set(LZ4_INCLUDE_DIRS ${PC_LZ4_INCLUDE_DIRS})
-else()
-    message(WARNING "liblz4 not found using pkg-config. Ensure it is installed.")
+  set(LZ4_LIBRARIES ${PC_LZ4_LIBRARIES})
+  set(LZ4_INCLUDE_DIRS ${PC_LZ4_INCLUDE_DIRS})
 endif()
 
 find_path(LZ4_INCLUDE_DIR
-    NAMES lz4.h
-    HINTS ${PC_LZ4_INCLUDEDIR} ${CMAKE_SOURCE_DIR}/include /usr/include/liblz4 /usr/local/include
+  NAMES lz4.h
+  HINTS ${PC_LZ4_INCLUDEDIR} ${CMAKE_SOURCE_DIR}/include /usr/include /usr/local/include
 )
 
-find_library(LZ4_LIBRARY
-    NAMES lz4
-    HINTS ${PC_LZ4_LIBDIR} ${CMAKE_SOURCE_DIR}/lib /usr/lib /usr/local/lib
-)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-    lz4
-    REQUIRED_VARS LZ4_INCLUDE_DIR LZ4_LIBRARY
-)
-
-if (lz4_FOUND)
-    message(STATUS "liblz4 found:")
-    message(STATUS "  Include dirs: ${LZ4_INCLUDE_DIR}")
-    message(STATUS "  Libraries: ${LZ4_LIBRARY}")
-else()
-    message(FATAL_ERROR "liblz4 not found! Please ensure the library is installed.")
+if (LZ4_INCLUDE_DIR)
+  message(STATUS "Found LZ4 include directory: ${LZ4_INCLUDE_DIR}")
 endif()
 
-mark_as_advanced(LZ4_INCLUDE_DIR LZ4_LIBRARY)
+find_library(LZ4_LIBRARY
+  NAMES lz4
+  HINTS ${PC_LZ4_LIBDIR} ${CMAKE_SOURCE_DIR}/lib /usr/lib /usr/local/lib
+)
+
+if (LZ4_LIBRARY)
+  message(STATUS "Found LZ4 library: ${LZ4_LIBRARY}")
+endif()
+
+if (LZ4_INCLUDE_DIR AND LZ4_LIBRARY)
+  set(HAVE_LZ4 TRUE)
+  message(STATUS "LZ4 is found and usable.")
+
+  set(LZ4_LIBRARIES ${LZ4_LIBRARY})
+  set(LZ4_INCLUDE_DIRS ${LZ4_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(
+  LZ4_INCLUDE_DIR
+  LZ4_LIBRARY
+)
+
+if (HAVE_LZ4)
+  add_library(lz4::lz4 UNKNOWN IMPORTED)
+  set_target_properties(lz4::lz4 PROPERTIES
+    IMPORTED_LOCATION "${LZ4_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${LZ4_INCLUDE_DIR}"
+  )
+endif()
