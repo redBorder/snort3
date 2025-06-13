da7e813efaf7776b7689fffd4df38b49369fd7fd
Miguel Alvarez <malvarez@redborder.com>
2025-06-13 17:09:58 +0100

    Delete FindAWS

--- cmake/include_libraries.cmake
+++ cmake/include_libraries.cmake
@@ -13,7 +13,6 @@ find_package(ZLIB REQUIRED)
 find_package(geoLite2pp REQUIRED)
 find_package(Rdkafka REQUIRED)
 find_package(CPR QUIET)
-find_package(AWS QUIET)
 
 if (ENABLE_UNIT_TESTS)
     find_package(CppUTest REQUIRED)
