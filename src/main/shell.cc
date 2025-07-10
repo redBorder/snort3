ae4a0a4cd5985aee257c02efa321f73a3ecd4fa0
Miguel Alvarez <malvarez@redborder.com>
2025-07-10 15:32:58 +0100

    go back to libml_version()

--- src/main/shell.cc
+++ src/main/shell.cc
@@ -165,7 +165,7 @@ static void install_dependencies_strings(Shell* sh, lua_State* L)
     vs.push_back(lzma_version_string());
 #endif
 #ifdef HAVE_LIBML
-    vs.push_back(libml::version());
+    vs.push_back(libml_version());
 #endif
 
     lua_createtable(L, 0, vs.size());
